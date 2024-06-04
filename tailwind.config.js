4272c4e12d9daf0a7c25c248c97043b8ad128bd2
Poonam Gaigole <poonam.gaigole@axelerant.com>
2024-06-04 16:08:36 +0530

    Customize tailwind colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
   content: ["./src/**/*.{html,ts,scss}"],
   theme: {
     extend: {},
+    colors: {
+      blue: {
+        900: "#003B68",
+      },
+      white: "#ffffff",
+    },
   },
   plugins: [],
 };
