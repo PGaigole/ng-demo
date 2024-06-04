3bc70130d593bd26181efa116db90effc82d757e
Poonam Gaigole <poonam.gaigole@axelerant.com>
2024-06-04 15:44:50 +0530

    Product promo component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductPromoComponent } from './product-promo/product-promo.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProductPromoComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'ng-demo';
---
+++ src/app/product-promo/product-promo.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-promo',
+  standalone: true,
+  templateUrl: './product-promo.component.html',
+  styleUrl: './product-promo.component.scss',
+})
+export class ProductPromoComponent {}
